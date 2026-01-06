183a2793774f2cb424244d20b5cf10c9771dd89c
Laine Viestenz <laineviestenz@gmail.com>
2026-01-05 19:51:04 -0600

    fixed critical math typo and moved time to methods

--- ball_drop.py
+++ ball_drop.py
@@ -3,23 +3,23 @@
 class FallingObject():
     """a model of a falling object, where the height, fall distance, and 
     fall time can be accessed through methods. All values must be represented
-    in meters and seconds"""
-    def __init__ (self, height, time):
+    in meters and seconds. The initial height is defined when the function is
+    created, while time is given when the method is used."""
+    def __init__ (self, height):
         self.height = height
-        self.time = time
     
-    def get_height(self):
+    def get_height(self, time):
         #calculate height based on fall time
-        height = round(self.height - (4.905*(self.time**2)), 3)
+        height = round(self.height - (4.905*(time**2)), 3)
         # height can not be negative
         if height < 0:
             height = 0
         #round and display the height
         print('The ball is ' + str(height) + ' meters from the ground.')
 
-    def get_distance(self):
+    def get_distance(self, time):
         #get distance based on fall time
-        distance = round(4.905*(self.time**2), 3)
+        distance = round(4.905*(time**2), 3)
         #can't fall further than initial height
         if distance > self.height:
             distance = self.height
@@ -28,10 +28,10 @@ class FallingObject():
     def get_fall_time(self):
         """finds the fall time regardless of the time value given"""
         #find the time that it takes for the height to equal zero
-        time = round((self.height/4.905)**0. , 3)
+        time = round((self.height/4.905)**0.5 , 3)
         print('The ball takes ' + str(time) + ' seconds to hit the ground.')
 
-ball = FallingObject(100, 4)
-ball.get_height()
-ball.get_distance()
+ball = FallingObject(100)
+ball.get_height(3)
+ball.get_distance(3)
 ball.get_fall_time()
