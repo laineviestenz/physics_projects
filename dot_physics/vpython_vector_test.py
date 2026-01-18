d151453b3002434604327869ebd44f0053ddb2d9
Laine Viestenz <laineviestenz@gmail.com>
2026-01-17 20:49:22 -0600

    finished 1st part of tutorial with basics

---
+++ dot_physics/vpython_vector_test.py
@@ -0,0 +1,23 @@
+from vpython import *
+A = vector(1, 3, -1)
+print(A)
+
+B = vector(0, 1, -4)
+print(B)
+
+C = A+B
+print(C)
+
+#This gives the x component of the vector
+print(C.x)
+
+#gives the magnitude of C
+print(mag(C))
+
+#2 different ways to print the unit vector version
+print(hat(C))
+print(norm(C))
+
+#print dot and cross products
+print(dot(A, B))
+print(cross(A, B))
