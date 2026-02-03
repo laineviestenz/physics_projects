f7bbca2e5216f764a80fdbc9fc54cf7dec07c2c5
Laine Viestenz <laineviestenz@gmail.com>
2026-02-02 23:48:04 -0600

    finished best angle calculation

--- dot_physics/3d_objects_projectile_motion/optimize_angle.py
+++ dot_physics/3d_objects_projectile_motion/optimize_angle.py
@@ -1,17 +1,31 @@
 from vpython import *
 import time
+"""credit: DotPhysics | while I modified or changed most lessons from this video
+series, this is very similar, although not exactly, to the code he wrote"""
 
+g1 = graph(xtitle="theta", ytitle ="range")
+f1 = gcurve(color=color.blue)
 def prange(ttheta, v0, r0):
     g = vector(0,-9.8,0)
     m = 0.05
     p = m*v0*vector(cos(ttheta), sin(ttheta), 0)
     t = 0
     dt = 0.001
+    r=r0
     while r.y >= 0:
         F = m*g
         p += F*dt
         r += p*dt/m
         t += dt
-    return(r,x)
-print("range = " + str(r.x), ' m')
+    return(r.x)
+
+theta = 1*pi/180
+dtheta = 1*pi/180
+vstart = 7
+
+while theta < 89*pi/180:
+    trange = prange(theta, vstart, vector(0,8,0))
+    f1.plot(theta*180/pi, trange)
+    theta += dtheta
+
 time.sleep(10)
