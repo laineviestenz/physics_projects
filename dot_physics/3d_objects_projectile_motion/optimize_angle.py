aca823e7660a5dac27d980156777c45add001751
Laine Viestenz <laineviestenz@gmail.com>
2026-02-05 00:09:37 -0600

    start of reorganizing projectile motion

--- dot_physics/3d_objects_projectile_motion/optimize_angle.py
+++ dot_physics/3d_objects_projectile_motion/optimize_angle.py
@@ -3,35 +3,49 @@ import time
 """credit: DotPhysics | while I modified or changed most lessons from this video
 series, this is very similar, although not exactly, to the code he wrote"""
 
-g1 = graph(xtitle="theta", ytitle ="range")
-f1 = gcurve(color=color.blue)
-def prange(ttheta, v0, r0):
+def get_range(angle, v0, initial_position):
+    """run until the ball reaches zero, and return the position(range of the ball)"""
     g = vector(0,-9.8,0)
-    m = 0.05
-    p = m*v0*vector(cos(ttheta), sin(ttheta), 0)
-    t = 0
+    mass = 0.05
+    momentum = mass*v0*vector(cos(angle), sin(angle), 0)
     dt = 0.001
-    r=r0
-    while r.y >= 0:
-        F = m*g
-        p += F*dt
-        r += p*dt/m
-        t += dt
-    return(r.x)
+    position=initial_position
+    while position.y >= 0:
+        force = mass*g
+        momentum += force*dt
+        position += momentum*dt/mass
+    return(position.x)
 
-theta = 1*pi/180
-dtheta = 1*pi/180
-vstart = 8
+def plot_best_angle():
+    #initialize the graph and curve
+    g1 = graph(xtitle="theta", ytitle ="range")
+    f1 = gcurve(color=color.blue)
+    theta = 0*pi/180
+    dtheta = 1*pi/180
+    v0 = 8
 
-mrange = 0
-mangle = 0
+    while theta < 89*pi/180:
+        trange = get_range(theta, v0, vector(0,0,0))
+        f1.plot(theta*180/pi, trange)
+        theta += dtheta
 
-while theta < 89*pi/180:
-    trange = prange(theta, vstart, vector(0,0,0))
-    if trange > mrange:
-        mrange = trange
-        mangle = theta
-    f1.plot(theta*180/pi, trange)
-    theta += dtheta
-print(str(mrange) + ' at ' + str(mangle*180/pi))
+def get_best_angle():
+    theta = 0*pi/180
+    dtheta = 1*pi/180
+    v0 = 8
+
+    maxrange = 0
+    maxangle = 0
+
+    while theta < 89*pi/180:
+        trange = get_range(theta, v0, vector(0,0,0))
+        if trange > maxrange:
+            maxrange = trange
+            maxangle = theta
+        theta += dtheta
+    print(str(maxrange) + ' at ' + str(maxangle*180/pi))
+
+
+get_best_angle()
+plot_best_angle()
 time.sleep(10)
