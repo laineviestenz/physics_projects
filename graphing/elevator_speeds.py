0146a4d7b5b87c2390a990bd95f2d1868dc4553a
Laine Viestenz <laineviestenz@gmail.com>
2026-02-06 20:28:36 -0600

    added error handling only for ValueError

--- graphing/elevator_speeds.py
+++ graphing/elevator_speeds.py
@@ -9,11 +9,24 @@ while True:
     position = input("Enter Position: ")
     if position == 'x':
         break
+    try:
+        float(position)
+    except ValueError:
+        print('Please enter a valid input.')
+        position = input("Enter Position: ")
+    else:
+        positions.append(position)
+    
     time = input('Enter Time: ')
     if time == 'x':
         break
-    positions.append(position)
-    times.append(time)
+    try:
+        float(time)
+    except ValueError:
+        print('Please enter a valid input.')
+        position = input("Enter Time: ")
+    else:
+        times.append(time)
 
 plt.style.use('classic')
 fig, ax = plt.subplots()
