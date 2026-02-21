d3fb1a3cf4b26ee26ed04f22376b551ac8037289
Laine Viestenz <laineviestenz@gmail.com>
2026-02-20 20:55:11 -0600

    fixed get_range(need conver deg -> rad)

--- dot_physics/3d_objects_projectile_motion/optimize_angle_w_resistance_matplotlib.py
+++ dot_physics/3d_objects_projectile_motion/optimize_angle_w_resistance_matplotlib.py
@@ -9,10 +9,10 @@ def get_range(theta, velocity_i, mass, radius, initial_position=np.array([0,0,0]
     cross_area = pi*radius**2
     rho = 1.2
     drag_coefficient = 0.47
-    momentum = mass*velocity_i*np.array([math.cos(theta), math.sin(theta), 0])
+    momentum = mass*velocity_i*np.array([math.cos(theta*pi/180), math.sin(theta*pi/180), 0])
     position = initial_position
     height = initial_position[1]
-    dt = 0.1
+    dt = 0.01
     while height >= 0:
         #calculate the net force vector using the quadratic drag formula
         Fnet = mass*gravity - 0.5*rho*cross_area*drag_coefficient*(np.linalg.norm(momentum/mass))**2*(momentum/np.linalg.norm(momentum))
@@ -21,6 +21,5 @@ def get_range(theta, velocity_i, mass, radius, initial_position=np.array([0,0,0]
         #P/m = velocity; distance = velocity*time
         position = position + ((momentum/mass) * dt)
         height = position[1]
-    return position[1]
+    return position[0]
 
-print(get_range(23, 10, 2, 0.02))
