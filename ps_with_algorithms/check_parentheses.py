2a3ef551d1ec78b53d998595c68501cbeb5be4c4
Laine Viestenz <laineviestenz@gmail.com>
2026-03-31 13:07:39 -0500

    started parentheses check function

---
+++ ps_with_algorithms/check_parentheses.py
@@ -0,0 +1,13 @@
+from stack_as_list import Stack
+
+def check_parentheses (string):
+    string = list(string)
+    for i in string:
+        if i == '(':
+            pass
+        if i == '(':
+            pass
+    return string
+
+print(check_parentheses('(0)()())(())'))
+
