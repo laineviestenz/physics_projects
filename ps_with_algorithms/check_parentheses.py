f981c76af8cbf4a757bc6924900adf0822db1875
Laine Viestenz <laineviestenz@gmail.com>
2026-04-07 14:55:13 -0500

    not working, updated to using stacks

--- ps_with_algorithms/check_parentheses.py
+++ ps_with_algorithms/check_parentheses.py
@@ -1,13 +1,23 @@
 from stack_as_list import Stack
 
 def check_parentheses (string):
+    """new method will use stacks for a much cleaner solution than I was
+    previously working on."""
+    """If there is a left parenthese, push it to the stack if there is a right
+    parenthese, pop from the stack. After going through the entire list, check
+    if the stack is empty. Also, if the stack is empty before the end, return 
+    false"""
     string = list(string)
+    stack = Stack()
     for i in string:
         if i == '(':
-            pass
+            stack.push(i)
         if i == '(':
-            pass
-    return string
+            if not stack.is_empty():
+                stack.pop(i)
+            else:
+                return False
+    return stack.is_empty()
 
 print(check_parentheses('(0)()())(())'))
 
