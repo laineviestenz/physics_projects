b0d21eb6c5be938edcfa03ec5fb3dbd4c1e2c741
Laine Viestenz <laineviestenz@gmail.com>
2026-04-13 22:47:12 -0500

    started changing logic

--- ps_with_algorithms/extended_symbol_check.py
+++ ps_with_algorithms/extended_symbol_check.py
@@ -2,6 +2,9 @@
 ITS OWN STACK, AND ADDITIONAL LOGIC FOR QUOTES, RAN OUT OF TIME WILL HAVE TO 
 FIX LATER!"""
 
+"""New note, that is not the best way to do it, use one stack with a function that
+check if the symbol is correct. Ran out of time again :("""
+
 from stack_as_list import Stack
 
 def check_symbols (string):
@@ -10,35 +13,24 @@ def check_symbols (string):
     string = list(string)
     stack = Stack()
     for i in string:
-        if i == '(':
+        if i in '({[':
             stack.push(i)
-        elif i == ')':
+        elif i in '}])':
             if not stack.is_empty():
                 stack.pop()
             else:
                 return False
         
-        elif i == '{':
-            stack.push(i)
-        elif i == '}':
-            if not stack.is_empty():
-                stack.pop()
-            else:
-                return False
-
-        elif i == '[':
-            stack.push(i)
-        elif i == ']':
-            if not stack.is_empty():
-                stack.pop()
-            else:
-                return False
-            
         elif i == '"' and stack.size() % 2 == 0:
             stack.push(i)
 
         
     return stack.is_empty()
+
+def matches(left_symbol, right_symbol):
+    left = "([{"
+    right = ")]}"
+    return left.index == right.index
 
 print(check_symbols('()(())()()()()()'))
 
