c3f8c6d6cdf775c063fd24e1694b597b1a0e5296
Laine Viestenz <laineviestenz@gmail.com>
2026-04-16 23:14:43 -0500

    moved return to address other letters in string

--- ps_with_algorithms/extended_symbol_check.py
+++ ps_with_algorithms/extended_symbol_check.py
@@ -1,10 +1,3 @@
-"""MAJOR NOTE! THIS IS NOT ACTUALLY EVEN CLOSE TO RIGHT, EACH SYMBOL NEEDS
-ITS OWN STACK, AND ADDITIONAL LOGIC FOR QUOTES, RAN OUT OF TIME WILL HAVE TO 
-FIX LATER!"""
-
-"""New note, that is not the best way to do it, use one stack with a function that
-check if the symbol is correct. Ran out of time again :("""
-
 from stack_as_list import Stack
 
 def check_symbols (string):
@@ -20,9 +13,7 @@ def check_symbols (string):
                 if matches(stack.peek(), i):
                     stack.pop()
                 else:
-                    return False
-        else:
-            return False        
+                    return False        
     return stack.is_empty()
 
 def matches(left_symbol, right_symbol):
@@ -30,5 +21,4 @@ def matches(left_symbol, right_symbol):
     right = [')', ']', '}']
     return left.index(left_symbol) == right.index(right_symbol)
 
-print(check_symbols('()(]'))
-
+print(check_symbols('()[]'))
