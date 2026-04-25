ba2c74182a917976ceefaa65571e20e2f358c185
Laine Viestenz <laineviestenz@gmail.com>
2026-04-25 13:00:43 -0500

    Simplified left sum with new way, still off by 1

--- integrating/riemann_sum.py
+++ integrating/riemann_sum.py
@@ -20,65 +20,60 @@ def riemann(function, lower_bound, upper_bound, precision, type = 'center'):
 
     if type.lower() == 'left' or type.lower() == 'l':
         x = lower_bound
-        while x <= upper_bound - dx:
-            total += eval(function)*dx
-            x += dx
-        print('the left sum is: ', total * negative)
-        #begin graphing function
-        fig, ax = plt.subplots()
-        c = [i for i in range(lower_bound, upper_bound, dx)]
-        d = []
-        
-        for i in range (lower_bound, upper_bound, dx):
-            x=i
-            d.append(eval(function))
-        
-        plt.plot(c,d)
-        for x in range(lower_bound, upper_bound-1):
-            plt.bar(x, d[x], dx, align='edge')
-        plt.show()
+        while x <= (upper_bound-dx):
+            plt.bar(x, eval(function), dx, align = 'edge')
+            total += dx * eval(function)
+            x+=dx
+#begin graphing function
+    c = [i for i in range(lower_bound, upper_bound, dx)]
+    d = []
     
-    elif type.lower() == 'right' or type.lower() == 'r':
-        x = lower_bound + dx
-        while x <= upper_bound:
-            total += eval(function)*dx
-            x += dx
-        print('the right sum is: ', total * negative)
+    for i in range (lower_bound, upper_bound, dx):
+        x=i
+        d.append(eval(function))
+    plt.plot(c, d)
+    plt.show()
+    
+#     elif type.lower() == 'right' or type.lower() == 'r':
+#         x = lower_bound + dx
+#         while x <= upper_bound:
+#             total += eval(function)*dx
+#             x += dx
+#         print('the right sum is: ', total * negative)
        
-        #begin graphing function
-        fig, ax = plt.subplots()
-        c = [i for i in range(lower_bound+dx, upper_bound, dx)]
-        d = []
+#         #begin graphing function
+#         fig, ax = plt.subplots()
+#         c = [i for i in range(lower_bound+dx, upper_bound, dx)]
+#         d = []
         
-        for i in range (lower_bound+dx, upper_bound, dx):
-            x=i
-            d.append(eval(function))
-            print(d)
-        plt.plot(c,d)
-        for x in range(lower_bound + dx, upper_bound, dx):
-            print(x)
-            plt.bar(x-dx, d[x-1], dx, align='edge')
-        plt.show()
+#         for i in range (lower_bound+dx, upper_bound, dx):
+#             x=i
+#             d.append(eval(function))
+#             print(d)
+#         plt.plot(c,d)
+#         for x in range(lower_bound + dx, upper_bound, dx):
+#             print(x)
+#             plt.bar(x-dx, d[x-1], dx, align='edge')
+#         plt.show()
     
 
-    else:
-        x = lower_bound + (dx*0.5)
-        while x <= upper_bound - (dx*0.5):
-            total += eval(function)*dx
-            x += dx
-        print('the center sum is: ', total * negative)
-        #begin graphing function
-        fig, ax = plt.subplots()
-        c = [i for i in range(lower_bound, upper_bound, dx)]
-        d = []
+#     else:
+#         x = lower_bound + (dx*0.5)
+#         while x <= upper_bound - (dx*0.5):
+#             total += eval(function)*dx
+#             x += dx
+#         print('the center sum is: ', total * negative)
+#         #begin graphing function
+#         fig, ax = plt.subplots()
+#         c = [i for i in range(lower_bound, upper_bound, dx)]
+#         d = []
         
-        for i in range (lower_bound, upper_bound, dx):
-            x=i+0.5*dx
-            d.append(eval(function))
+#         for i in range (lower_bound, upper_bound, dx):
+#             x=i+0.5*dx
+#             d.append(eval(function))
         
-        plt.plot(c,d)
-        for x in range(lower_bound, upper_bound):
-            plt.bar(x, d[x], dx, align='center')
-        plt.show()
-
-riemann("x+2", 0, 10, 1, type='left')
+#         plt.plot(c,d)
+#         for x in range(lower_bound, upper_bound):
+#             plt.bar(x, d[x], dx, align='center')
+#         plt.show()
+riemann("x**2", 0, 10, 1, type='left')
