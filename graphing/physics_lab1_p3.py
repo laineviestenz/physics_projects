980f4f9d2b3cecf285940f0675e525a79d4de81b
Laine Viestenz <laineviestenz@gmail.com>
2026-09-16 09:27:55 -0500

    added linear fit line

--- graphing/physics_lab1_p3.py
+++ graphing/physics_lab1_p3.py
@@ -7,9 +7,12 @@ time = np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45,
 voltage = np.array([999, 830, 743, 656, 580, 513, 453, 401, 350, 310,
                     269, 243, 215, 190, 167, 150, 131, 114, 103])
 
+m,b = np.polyfit(time, np.log(voltage), 1)
 
-
+plt.scatter(time, voltage)
+plt.plot(time, np.exp(m*time+b))
 plt.yscale('log')
 plt.grid(True, which='both')
-plt.plot(time, voltage)
+plt.xlim(0,right=None)
+
 plt.show()
